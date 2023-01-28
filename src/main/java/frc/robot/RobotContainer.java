609392ac700ca7f6418d0011259c54a19f08a04b
fry42guy <47529295+fry42guy@users.noreply.github.com>
2023-01-27 21:16:45 -0600

    2023

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,13 +6,19 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+
+import javax.swing.ButtonGroup;
+
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.Button;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.commands.DefaultDriveCommand;
 import frc.robot.subsystems.DrivetrainSubsystem;
+import frc.robot.subsystems.RoboArm;
+
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -25,6 +31,7 @@ public class RobotContainer {
   private final DrivetrainSubsystem m_drivetrainSubsystem = new DrivetrainSubsystem();
 
   private final XboxController m_controller = new XboxController(0);
+  private final RoboArm m_RobotArm = new RoboArm();
 
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
@@ -54,10 +61,21 @@ public class RobotContainer {
    */
   private void configureButtonBindings() {
     // Back button zeros the gyroscope
-    new Button(m_controller::getBackButton)
+    new Trigger(m_controller::getBackButton)
             // No requirements because we don't need to interrupt anything
-            .whenPressed(m_drivetrainSubsystem::zeroGyroscope);
+            .onTrue(m_drivetrainSubsystem.zeroGyroscope());  //############################################
+            
+
+    new Trigger(m_controller::getAButton)
+            
+      .onTrue(m_RobotArm.Arm_Motor_Forward())
+      .onFalse(m_RobotArm.Arm_Motor_Stop());
+      
+          
+
+
   }
+
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
